357409421e74790f9bb2316dcbf08824d88b1d19
tienmanh294 <manh.dang2904@hcmut.edu.vn>
2022-04-13 19:56:10 +0700

    add more device

--- src/components/devices/Device.js
+++ src/components/devices/Device.js
@@ -1,20 +1,26 @@
 import Card from "../ui/Card";
 import classes from "./Device.module.css";
+import {useRef} from 'react'
 function Device(props) {
-  // const contentResponse=
-  function submitHandler(){
-
+  const buttonInputRef=useRef();
+  function handleChange(event){
+    event.preventDefault();
+    const userButtonRef=buttonInputRef.current.value;
+    const changedData={
+      changed:userButtonRef
+    }
+    props.onChangeDevice(changedData);
   }
   return (
     <Card>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <form className={classes.form}>
         <div>
           <label className={classes.name}>{props.deviceName}
           </label>
           </div>
-        <textarea rows="10" cols="50" required id="description"></textarea>
+        <textarea rows="10" cols="50" required id="description">{props.data}</textarea>
         <label className={classes.switch}>
-          <input type="checkbox"></input>
+          <input type="checkbox" onChange={handleChange}></input>
           <span className={`${classes.slider} ${classes.round}` }></span>
         </label>
       </form>
--- src/pages/devices.js
+++ src/pages/devices.js
@@ -1,11 +1,32 @@
 import Device from "../components/devices/Device";
+import {useState} from 'react'
 function DevicesPage(){
+    const[serverData,setServerData]=useState({});
+    function sendRequest(changedData){
+        fetch(
+            "https://react-getting-started-d2ed4-default-rtdb.firebaseio.com/meetups.json",
+            {
+              method: "POST",
+              body: JSON.stringify(changedData),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          ).then((response)=>{
+             return response.json()
+          }).then((data)=>{
+            setServerData(data);
+          });
+    }
     return(
         <section>
             <h1>Manage devices</h1>
-            <Device deviceName="DHT11"></Device>
+            <Device deviceName="DHT11" ></Device>
             <Device deviceName="Gas sensor"></Device>
             <Device deviceName="Magetic Switch"></Device>
+            <Device deviceName="Buzzer" ></Device>
+            <Device deviceName="Single Push Button"></Device>
+            <Device deviceName="Light sensor"></Device>
         </section>
     );
 }
